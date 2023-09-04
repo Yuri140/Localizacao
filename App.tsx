da5e45254bc9f488794bd47d12f53605d127b02e
Yuri140 <yurisousa.carneiro2006@gmail.com>
2023-09-04 15:33:08 -0300

    Firebase inserido, porém dando erro db.collection

--- App.tsx
+++ App.tsx
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from 'react';
-
 import {
-  StyleSheet,
-  View,
-  Text,
-  Button
+  View, // container
+  Text, //texto normal
+  StyleSheet, // basicamente um css
+  TouchableOpacity, //vai funcionar como um botão
 } from 'react-native';
 import { UseLocation } from './src/hooks/UseLocation';
 
-
-
 const App = () => {
+  const [isCapturing, setIsCapturing] = useState(false);
+
   return (
     <View style={styles.container}>
-      <UseLocation />
+      <Text style={styles.title}>POC de Captura de Localização</Text>
+      {!isCapturing ? (
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => setIsCapturing(true)}
+        >
+          <Text style={styles.buttonText}>Iniciar Captura</Text>
+        </TouchableOpacity>
+      ) : (
+        <UseLocation />
+      )}
     </View>
   );
 }
@@ -23,9 +32,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+    backgroundColor: '#f0f0f0',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: '#F4A100',
+    padding: 10,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
 });
 
-
 export default App;
-

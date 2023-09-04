b2eecd0f0438a270934d79681dc991edce0ee0b5
Yuri140 <yurisousa.carneiro2006@gmail.com>
2023-09-04 14:41:46 -0300

    Tela inicial

---
+++ src/config/firebaseconfig.tsx
@@ -0,0 +1,22 @@
+
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB5hcN6my5n1nitG5Fhe6Oin9aO2uMDmlk",
+    authDomain: "projetoa8.firebaseapp.com",
+    projectId: "projetoa8",
+    storageBucket: "projetoa8.appspot.com",
+    messagingSenderId: "536026051099",
+    appId: "1:536026051099:web:ced2e37cba5d8b569efebe",
+    measurementId: "G-R94VH2NRF4"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+export const storage = getStorage(app);
+export default db;
